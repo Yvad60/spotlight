98ffa77beeaee83c58cd4229976327d84f7ea5a8
ivad <yvesyvad@gmail.com>
2023-06-20 22:26:53 +0200

    feat: migrate common page

--- src/common/styles.ts
+++ src/common/styles.ts
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 
-export const setCategoryClasses = (category, selectedCategory) =>
+export const setCategoryClasses = (category: string, selectedCategory: string) =>
   classNames("capitalize cursor-pointer hover:bg-[#f4efea] px-2 pt-2", {
     "text-yellow-700 border-b-current pb-3 border-b-[3px]": category === selectedCategory,
     "pb-2": category !== selectedCategory,

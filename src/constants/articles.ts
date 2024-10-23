5eb8a9b6b810b566ecf1236aadcac7f59bdd77e6
Ivad <yvesyvad@gmail.com>
2024-10-23 20:17:37 +0200

    chore: remove dummy article

--- src/constants/articles.ts
+++ src/constants/articles.ts
@@ -1,4 +1,4 @@
-export const categories = ["trending", "health", "business", "sports", "technology", 'Ivad'];
+export const categories = ["trending", "health", "business", "sports", "technology"];
 
 export const unfoundArticlesMessage =
   "Sorry, there were no articles that matched your selection. You need to adjust selection and try again.";
